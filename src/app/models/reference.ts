f3919ae3aba74218cae4ac7d483e9e567f53cd04
marbifer <35464670+marbifer@users.noreply.github.com>
2018-10-03 17:16:58 -0300

    mongo-db - Updated 5: Implementación de la BD con mongo

---
+++ src/app/models/reference.ts
@@ -0,0 +1,7 @@
+export class ReferenceModel {
+    _id: string;
+    select: string[];
+    codes: { code2: number };
+    currency: string[];
+}
+
--- src/app/services/references/references.service.ts
+++ src/app/services/references/references.service.ts
@@ -5,6 +5,7 @@ import { of } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
 import { References } from '../../interfaces/references.interface';
+import { ReferenceModel } from '../../models/reference';
 import { store } from '@angular/core/src/render3/instructions';
 
 @Injectable({
@@ -13,26 +14,49 @@ import { store } from '@angular/core/src/render3/instructions';
 export class ReferencesService {
 
   private referencesUrl = './assets/references.json';
+  readonly URL_API = 'http://localhost:3000/api/my-practice';
+
+  public selectedReference = ReferenceModel;
 
   private selectItemsSource = new BehaviorSubject<References | null>(null);
   public selectItemsChanges$ = this.selectItemsSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  public getReferences(): void {
+  /* public getReferences(): void {
     /* if (this.references) {
       return of(this.references);
     } */
 
-    this.http.get<References>(this.referencesUrl)
-      .subscribe(
-        data => {
-          console.log('request', JSON.stringify(data));
-          this.selectItemsSource.next(data);
-        },
-        catchError(this.handleError)
-      );
+  /* this.http.get<References>(this.referencesUrl)
+   .subscribe(
+     data => {
+       console.log('request', JSON.stringify(data));
+       this.selectItemsSource.next(data);
+     },
+     catchError(this.handleError)
+   );
+}  */
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  // Consulta con mongo:
+  getReferences() {
+    return this.http.get(this.URL_API);
   }
+
+  postReference(Reference: ReferenceModel) {
+    return this.http.post(this.URL_API, Reference);
+  }
+
+  putEmployee(Reference: ReferenceModel) {
+    return this.http.put(this.URL_API + `/${Reference._id}`, Reference);
+  }
+
+  deleteEmployee(_id: string) {
+    return this.http.delete(this.URL_API + `/${_id}`);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
